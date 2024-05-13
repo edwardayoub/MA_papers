16707a3ae0b9c3f21522e87b9c639a720c8060a5
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-05-13 13:56:42 -0500

    Update Pubmed.py

--- pages/Pubmed.py
+++ pages/Pubmed.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 from Bio import Entrez
-import csv
 import pandas as pd
 
 # Set your email here to let NCBI know who you are
@@ -44,27 +43,6 @@ def search_pubmed(query):
     pubmed_ids = record['IdList']
     return pubmed_ids
 
-def save_to_csv(articles, filename):
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        fieldnames = ["PMID", "Title", "Authors", "Abstract", "Link"]
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        for article in articles:
-            writer.writerow(article)
-
-def save_articles_as_txt(articles):
-    folder_path = 'articles'
-    os.makedirs(folder_path, exist_ok=True)  # Create the folder if it does not exist
-    
-    for article in articles:
-        file_path = os.path.join(folder_path, f"{article['PMID']}.txt")
-        if not os.path.exists(file_path):  # Check if file already exists
-            with open(file_path, 'w', encoding='utf-8') as file:
-                file.write(f"Title: {article['Title']}\n")
-                file.write(f"Authors: {article['Authors']}\n")
-                file.write(f"Abstract: {article['Abstract']}\n")
-                file.write(f"Link: {article['Link']}\n")
-
 # Streamlit application
 st.title("PubMed Article Search")
 
@@ -76,9 +54,6 @@ if st.button("Search"):
         articles = fetch_articles(pubmed_ids)
         df = pd.DataFrame(articles)
         st.dataframe(df)
-
-        save_to_csv(articles, 'MA_articles.csv')
-        save_articles_as_txt(articles)
         st.success("Articles have been fetched and saved.")
     else:
         st.warning("No articles found for the given keywords.")
